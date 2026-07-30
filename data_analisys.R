1056308d8470626927ec1a73c4b6d8a1977d129d
Gabriela Dezotti <gabriela.dezotti@unesp.br>
2026-07-30 16:09:00 -0300

    Arrumei os trem menino

--- data_analisys.R
+++ data_analisys.R
@@ -13,6 +13,9 @@ library(tidyverse)
 library(lmerTest)
 library(AICcmodavg)
 library(here)
+library(treemap)
+library(treemapify)
+
 
 
 dados = read_csv(here("data_for_collaborators.csv"))
@@ -23,45 +26,76 @@ dados = dados |>
          s_nat_rich_covar, s_inv_rich, s_inv_rel_abund, yrs_with_intro)) |>
 mutate(invasive_pa = ifelse(s_inv_rich == 0, 0, 1),
 invasive_pa = factor(invasive_pa, levels = c(0, 1), labels = c("Nativa", "Não-nativa")))
+# Classificando os sítios como com presença ou não de espécies não-nativa
+
 
 # Primeira pergunta: Estabilidade total em comunidades nativas vs não-nativas
 
-#Boxplot
-
+#Boxplot simples
 dados |>
   ggplot(aes(x = invasive_pa, y = log_CVc, fill = invasive_pa)) +
   geom_boxplot() +
   labs(title = "Estabilidade total em comunidades nativas vs não-nativas")
 
+
 #Violin Chart
-
 dados %>%
+
   ggplot(aes(x = invasive_pa, y = log_CVc, fill = invasive_pa, shape = invasive_pa)) +
-  ggdist::stat_halfeye(adjust = .5, width = .3, show.legend = FALSE,
-                       .width = 0, justification = -.3, alpha = .9,
-                       point_colour = NA) + 
-  geom_boxplot(width = .1, outlier.shape = NA,
-               alpha = .5, show.legend = FALSE) +
-  gghalves::geom_half_point(side = "l", range_scale = .4,
-                            show.legend = FALSE, alpha = .5) +
-  labs(x = "", y = "log(CVc)", 
-       title = "Estabilidade total em comunidades nativas vs não-nativas") +
-  scale_fill_manual('', values = rev(wesanderson::wes_palette(n=2, name="Darjeeling1")), 
-                    labels = c("Nativa", "Não-nativa")) +
-  scale_shape_manual('', values = c(21, 23),
-                     labels = c("Nativa", "Não-nativa")) +
-  scale_color_manual('', values = rev(wesanderson::wes_palette(n=2, name="Darjeeling1")), 
-                     labels = c("Nativa", "Não-nativa")) +
+ 
+
+ geom_hline(yintercept = 0, linetype = "dashed", color = "#050404", 
+ linewidth = 0.8) +
+
+  ggdist::stat_halfeye(
+    adjust = .5, width = .3, show.legend = FALSE,
+    .width = 0, justification = -.3, alpha = .6,
+    point_colour = NA
+  ) + 
+
+  ggbeeswarm::geom_quasirandom(  # Troquei o gghalves por ggbeeswarm pra resolver seu problema ;)
+    width = .12,
+    alpha = .5,
+    size = 1.8,
+    show.legend = FALSE
+  ) +
+
+  geom_boxplot(
+    width = .1, outlier.shape = NA,
+    alpha = .75, show.legend = FALSE
+  ) +
+
+  labs(
+    x = "",
+    y = "log(CVc)"
+  ) +
+
+  scale_fill_manual(
+    '',
+    values = rev(wesanderson::wes_palette(n = 2, name = "GrandBudapest1")),
+    labels = c("Nativa", "Não-nativa")
+  ) +
+
+  scale_shape_manual(
+    '',
+    values = c(21, 23),
+    labels = c("Nativa", "Não-nativa")
+  ) +
+
+  scale_color_manual(
+    '',
+    values = rev(wesanderson::wes_palette(n = 2, name = "GrandBudapest1")),
+    labels = c("Nativa", "Não-nativa")
+  ) +
+
   theme_classic() +
-  theme(axis.text.y = element_text(color = "black", size = 20),
-        axis.text.x = element_text(color = "black", size = 20),
-        axis.title.y = element_text(size = 24, face = "bold"),
-        plot.title = element_text(size = 20, face = "bold"),
-        panel.border = element_rect(linewidth = 2, colour = "black",
-                                    fill = NA))
-
-
-# GABRIEL: Deixar esse grafico mais bonito, com cores diferentes (padrão de cores, mais limpo e com tema minimalista)
+  theme(
+    axis.text.y = element_text(color = "black", size = 26),
+    axis.text.x = element_text(color = "black", size = 30),
+    axis.title.y = element_text(size = 24, face = "bold"),
+    plot.title = element_text(size = 20, face = "bold"),
+    panel.border = element_rect(linewidth = 2, colour = "black", fill = NA)
+  )
 
 # Modelo: Estabilidade total em comunidades nativas vs não-nativas
 
@@ -70,8 +104,6 @@ summary(mod_pergunta)
 
 # GABRIEL: testar os diagnosticos do modelo e escrever uma explicação sobre o resultado do modelo, se a relação entre estabilidade
 # e diversidade é diferente para comunidades nativas e não-nativas.
-
-library(treemap)
 
 componentes_long = dados |>
   filter(!is.na(invasive_pa)) |>
@@ -103,34 +135,120 @@ nao_native <- componentes_long |>
   summarise(valor = mean(valor, na.rm = TRUE), .groups = "drop") |>
   mutate(magnitude = abs(valor))
 
-
-
-#Arrumar os graficos e vc vai usar o patchwork pra fazer uma figura unica com os dois treemaps
-
-# mapa1 + mapa2
-# annotatiom (a) (b) 
-
-treemap(nativa,
+# Nativa
+treemap(
+  nativa,
   index = "componente",
   vSize = "magnitude",
   vColor = "valor",
   type = "value",
   palette = "RdBu",
-  title = "Componentes de estabilidade — sem invasoras",
-  fontsize.labels = 12,
-  border.col = "white"
+  title = "",
+  title.legend = "",
+  fontsize.labels = 18,
+  fontcolor.labels = "white",
+  fontface.labels = c("bold"),
+  border.col = "white",
+  border.lwds = c(2, 2),
+  align.labels = list(
+    c("center", "center"),
+    c("center", "center")
+  ),
+  bg.labels = "transparent",
+  position.legend = "none"
 )
 
+# treemapify()
+p_nativa =
+ggplot(
+  nativa,
+  aes(
+    area  = magnitude,
+    fill  = valor,
+    label = componente
+  )
+) +
+  geom_treemap(color = "white", size = 10) +
+  geom_treemap_text(
+    colour = "white",
+    size   = 1.2,
+    fontface = "bold",
+    place  = "centre",
+    grow   = TRUE
+  ) +
+  scale_fill_distiller(palette = "RdBu") +
+  labs(
+    title = "Comunidades nativas",
+    fill  = "Value"
+  ) +
+  theme_void() +
+  theme(
+    plot.title = element_text(
+      hjust = 0.5,
+      face  = "bold",
+      size  = 30
+    ),
+    legend.position = "none"
+  )
+
+
+# Não-nativa
 treemap(nao_native,
   index = "componente",
   vSize = "magnitude",
   vColor = "valor",
   type = "value",
   palette = "RdBu",
-  title = "Componentes de estabilidade — com invasoras",
-  fontsize.labels = 12,
-  border.col = "white"
+  title = "",
+  title.legend = "",
+  fontsize.labels = 18,
+  fontcolor.labels = "white",
+  fontface.labels = c("bold"),
+  border.col = "white",
+  border.lwds = c(2, 2),
+  align.labels = list(
+    c("center", "center"),
+    c("center", "center")
+  ),
+  bg.labels = "transparent",
+  position.legend = "none"
 )
+
+# treemapify()
+p_naonativa =
+ggplot(
+  nao_native,
+  aes(
+    area  = magnitude,
+    fill  = valor,
+    label = componente
+  )
+) +
+  geom_treemap(color = "white", size = 2) +
+  geom_treemap_text(
+    colour = "white",
+    size   = 1.2,
+    fontface = "bold",
+    place  = "centre",
+    grow   = TRUE
+  ) +
+  scale_fill_distiller(palette = "RdBu") +
+  labs(
+    title = "Comunidades não nativas",
+    fill  = "Value"
+  ) +
+  theme_void() +
+  theme(
+    plot.title = element_text(
+      hjust = 0.5,
+      face  = "bold",
+      size  = 30
+    ),
+    legend.position = "none"
+  )
+
+
+p_nativa + p_naonativa
 
 # Testar os modelos de cada componente - e avaliar os resultadfos
 
